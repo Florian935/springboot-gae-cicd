68412169470e0d2a4512e6829632424d8f4723da
Florian <florian.martin63000@gmail.com>
2021-03-24 01:00:41 +0100

    Feat: initial commit

---
+++ src/main/java/com/florian935/gae/cicd/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.florian935.gae.cicd.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String greeting() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/florian935/gae/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.gae.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
